6e2fef400021176978367cb379697ddec773d549
Adam <alloyddesign@gmail.com>
2017-04-05 13:15:34 +0100

    look and feel

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
     $('#generate').click(function(){
+        var output = $('#outputText');
+        output.hide();
         var getUrl = window.location;
         var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
         $.get(baseUrl + 'generate', function (data) {
-            $("#output").html(data);
+            output.html(data);
+            output.fadeIn();
         });
     });
 });
