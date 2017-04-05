b907d550cd60bb99d72b817d1328bf157e37b60a
Adam <alloyddesign@gmail.com>
2017-04-05 13:08:37 +0100

    look and feel

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function () {
+    $('#generate').click(function(){
+        var getUrl = window.location;
+        var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
+        $.get(baseUrl + 'generate', function (data) {
+            $("#output").html(data);
+        });
+    });
+});
--- routes/index.js
+++ routes/index.js
@@ -14,11 +14,14 @@ router.get('/generate', function(req, res){
 
     randPrefix = prefix.nouns[Math.floor(Math.random() * prefix.nouns.length)];
     randPrefix1 = prefix.nouns[Math.floor(Math.random() * prefix.nouns.length)];
-
     randBuzzWord = buzz.nouns[Math.floor(Math.random() * buzz.nouns.length)];
 
-    res.json(randPrefix.charAt(0).toUpperCase() + randPrefix.slice(1) + " " +  randBuzzWord.charAt(0).toUpperCase() + randBuzzWord.slice(1) + " Generator");
+    res.json(cap(randPrefix) + " " +  cap(randPrefix1) + " " +  cap(randBuzzWord) + " Generator");
 
 });
+
+var cap = function(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+};
 
 module.exports = router;
