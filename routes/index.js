65a381a1673508c1c592565bf7997f41c8aa4576
Adam <alloyddesign@gmail.com>
2017-04-05 11:25:45 +0100

    deploy

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,17 @@ var prefix = require('../json/nouns.json');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', { title: 'Generator' });
+    res.render('index');
 });
 
 router.get('/generate', function(req, res){
 
     var randPrefix, randPrefix1;
 
-   // while((typeof randPrefix == 'undefined' && typeof randPrefix1 == 'undefined') && randPrefix != randPrefix1) {
-        randPrefix = prefix.nouns[Math.floor(Math.random() * prefix.nouns.length)];
-        randPrefix1 = prefix.nouns[Math.floor(Math.random() * prefix.nouns.length)];
-   // }
+    randPrefix = prefix.nouns[Math.floor(Math.random() * prefix.nouns.length)];
+    randPrefix1 = prefix.nouns[Math.floor(Math.random() * prefix.nouns.length)];
 
-
-    res.json(randPrefix + " " + randPrefix1 + " generator");
+    res.json(randPrefix.charAt(0).toUpperCase() + randPrefix.slice(1) + " " + randPrefix1.charAt(0).toUpperCase() + randPrefix1.slice(1) + " Generator");
 
 });
 
