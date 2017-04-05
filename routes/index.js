3ca5b96b58a85562e50e84c0c2f9348083e8adb5
Adam <alloyddesign@gmail.com>
2017-04-05 11:15:15 +0100

    deploy

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var prefix = require('../json/nouns.json');
-var suffix = require('../json/nouns2.json');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Generator' });
+    res.render('index', { title: 'Generator' });
 });
 
 router.get('/generate', function(req, res){
 
     var randPrefix, randPrefix1;
 
-  while((randPrefix == null && randPrefix2==null) && randPrefix!=randPrefix2) {
-      randPrefix = prefix.nouns[Math.floor(Math.random() * prefix.nouns.length)];
-      randPrefix2 = prefix.nouns[Math.floor(Math.random() * prefix.nouns.length)];
-  }
-  var randSuffix = suffix.nouns[Math.floor(Math.random() * suffix.nouns.length)];
+   // while((typeof randPrefix == 'undefined' && typeof randPrefix1 == 'undefined') && randPrefix != randPrefix1) {
+        randPrefix = prefix.nouns[Math.floor(Math.random() * prefix.nouns.length)];
+        randPrefix1 = prefix.nouns[Math.floor(Math.random() * prefix.nouns.length)];
+   // }
 
-  res.json(randPrefix + " " + randPrefix2 + " " + randSuffix + " generator");
+
+    res.json(randPrefix + " " + randPrefix1 + " generator");
 
 });
 
